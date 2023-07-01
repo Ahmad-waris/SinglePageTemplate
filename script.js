ed3c853a4e68f7e9eea28a7c2de1794045260683
Ahmad-waris <121746915+Ahmad-waris@users.noreply.github.com>
2023-07-01 07:48:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//       var scroll = $(window).scrollTop();
+//       if (scroll > 0) {
+//         $('#navbar').addClass('navbar-scrolled');
+//       } else {
+//         $('#navbar').removeClass('navbar-scrolled');
+//       }
+//     });
+//   });
